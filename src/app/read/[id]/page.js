2841b7b16cbbb46edb743fcc7a9420c668d49bce
SoonHwanKwon <hardtack6348@gmail.com>
2023-09-20 00:43:29 +0900

    create button and fixed create button bug

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,9 +1,10 @@
+// layout.js는 없어도 동작하지만 있으면 layout.js에 page.js가 들어가서 동작한다. 
 export default async function Read(props) {
     const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
     const topic = await resp.json();
     return(
     <>
-        <h2>{topic.tilte}</h2>
+        <h2>{topic.title}</h2>
         {topic.body}
     </>
     )

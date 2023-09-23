3c84d3b2ecc888b690c99cdf8d532c9f89dd16b4
SoonHwanKwon <hardtack6348@gmail.com>
2023-09-24 01:55:08 +0900

    finished update part

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,6 +1,7 @@
 // layout.js는 없어도 동작하지만 있으면 layout.js에 page.js가 들어가서 동작한다. 
 export default async function Read(props) {
-    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`
+    , {cache: 'no-store'});
     const topic = await resp.json();
     return(
     <>
